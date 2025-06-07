89b0ad0e8ebf8d94d0903cd23c983163db995f03
VenomAI <jht8287@gmail.com>
2025-06-07 14:24:21 +0000

    Initial project setup

--- src/project.v
+++ src/project.v
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Jake T.
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
+`timescale 1ns / 1ps
 
-module tt_um_example (
+module tt_um_venom_edlo (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,7 +18,7 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out  = ui_in + uio_in;  // Example: uo_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
